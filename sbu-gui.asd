ea4c147bd57c95af73389030388027fc5265b61d
Travis <tssund93@gmail.com>
2020-01-04 20:02:11 -0600

    init

---
+++ sbu-gui.asd
@@ -0,0 +1,9 @@
+(asdf:defsystem #:sbu-gui
+  :description "Describe tenhou-dl here"
+  :author "Your Name <your.name@example.com>"
+  :license  "Specify license here"
+  :version "0.0.1"
+  :serial t
+  :pathname "./src/"
+  :depends-on (:travv0.prelude)
+  :components ((:file "sbu-gui")))
---
+++ src/sbu-gui.lisp
@@ -0,0 +1,43 @@
+(defpackage :sbu-gui
+  (:use #:travv0.prelude #:capi))
+
+(in-package :sbu-gui)
+
+(define-interface window ()
+  ((game-title-width :initarg :game-title-width :initform 17)
+   (button-width :initarg :button-width :initform 12))
+  (:panes (list-buttons push-button-panel
+                        :items '(:backup :remove)
+                        :default-button :backup
+                        :print-function 'string-capitalize
+                        :layout-class 'column-layout
+                        :layout-args `(:visible-min-width (:character ,button-width)))
+          (game-list list-panel
+                     :items '("New...")
+                     :visible-min-width '(:character 40)
+                     :visible-min-height '(:character 10))
+          (game-name text-input-pane
+                     :title "Game Name"
+                     :title-args `(:visible-min-width (:character ,game-title-width)))
+          (game-save-path text-input-pane
+                          :title "Game Save Path"
+                          :title-args `(:visible-min-width (:character ,game-title-width))
+                          :file-completion t
+                          :directories-only t
+                          :editing-callback
+                          (lambda (pane type)
+                            (when (eql type :start)
+                              (text-input-pane-complete-text pane))))
+          (game-save-glob text-input-pane
+                          :title "Game Save Glob"
+                          :title-args `(:visible-min-width (:character ,game-title-width)))
+          (save-button push-button :data "Add" :visible-min-width `(:character ,button-width)))
+  (:layouts (main-layout column-layout '(games-layout game-edit-layout))
+            (games-layout row-layout '(game-list list-buttons))
+            (game-edit-layout column-layout
+                              '(game-name
+                                game-save-path
+                                game-save-glob
+                                save-button)
+                              :adjust :right))
+  (:default-initargs :title "Save Backup"))
