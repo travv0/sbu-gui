aba8bac104c44818aa51537d75849ff1bf5f47f6
Travis <tssund93@gmail.com>
2020-01-05 12:03:55 -0600

    adding, editing, and removing from list of games

--- src/sbu-gui.lisp
+++ src/sbu-gui.lisp
@@ -5,18 +5,34 @@
 
 (define-interface window ()
   ((game-title-width :initarg :game-title-width :initform 17)
-   (button-width :initarg :button-width :initform 12))
+   (button-width :initarg :button-width :initform 12)
+   (games :initarg :games
+          :initform (make-hash-table :test 'equal)
+          :reader games))
   (:panes
    (list-buttons push-button-panel
                  :items '(:backup :remove)
                  :default-button :backup
                  :print-function 'string-capitalize
                  :layout-class 'column-layout
-                 :layout-args `(:visible-min-width (:character ,button-width)))
+                 :layout-args `(:visible-min-width (:character ,button-width))
+                 :callbacks (list nil 'remove-game))
    (game-list list-panel
-              :items '("New...")
+              :items games
+              :items-map-function (lambda (ht f cr)
+                                    (funcall (if cr #'mapcar #'mapc)
+                                             f
+                                             (sort (hash-table-keys ht) #'string-lessp)))
+              :items-count-function (compose (curry #'+ 1) #'hash-table-count)
+              :items-get-function (lambda (ht index)
+                                    (if (= 0 index)
+                                        "New..."
+                                        (nth (1- index)
+                                             (sort (hash-table-keys ht) #'string-lessp))))
+              :test-function 'equal
               :visible-min-width '(:character 40)
-              :visible-min-height '(:character 10))
+              :visible-min-height '(:character 10)
+              :selection-callback 'select-game)
    (game-name text-input-pane
               :title "Game Name"
               :title-args `(:visible-min-width (:character ,game-title-width)))
@@ -30,7 +46,10 @@
    (game-save-glob text-input-pane
                    :title "Game Save Glob"
                    :title-args `(:visible-min-width (:character ,game-title-width)))
-   (save-button push-button :data "Add" :visible-min-width `(:character ,button-width)))
+   (save-button push-button :data "Add"
+                            :visible-min-width `(:character ,button-width)
+                            :callback-type :interface
+                            :callback 'add-game))
   (:layouts
    (main-layout column-layout '(games-layout game-edit-layout))
    (games-layout row-layout '(game-list list-buttons))
@@ -41,6 +60,71 @@
                        save-button)
                      :adjust :right))
   (:default-initargs :title "Save Backup"))
+
+(defun add-game (interface)
+  (bind (((:slots games game-list game-name game-save-path game-save-glob) interface)
+         ((:accessors (game-name text-input-pane-text)) game-name)
+         ((:accessors (game-save-path text-input-pane-text)) game-save-path)
+         ((:accessors (game-save-glob text-input-pane-text)) game-save-glob))
+    (setf (gethash game-name games) `(:save-path ,game-save-path
+                                      :save-glob ,game-save-glob)
+          (collection-items (slot-value interface 'game-list)) games
+          game-name ""
+          game-save-path ""
+          game-save-glob "")))
+
+(defun select-game (data interface)
+  (bind (((:slots games game-list game-name game-save-path game-save-glob save-button)
+          interface)
+         ((:accessors (game-name text-input-pane-text)) game-name)
+         ((:accessors (game-save-path text-input-pane-text)) game-save-path)
+         ((:accessors (game-save-glob text-input-pane-text)) game-save-glob)
+         ((:accessors (button-callback callbacks-selection-callback)
+                      (button-text item-data))
+          save-button)
+         ((:plist save-path save-glob) (gethash data games)))
+    (if (string= data "New...")
+        (setf game-name ""
+              game-save-path ""
+              game-save-glob ""
+              button-callback 'add-game
+              button-text "Add")
+        (setf game-name data
+              game-save-path save-path
+              game-save-glob save-glob
+              button-callback 'edit-game
+              button-text "Save"))))
+
+(defun edit-game (interface)
+  (bind (((:slots games game-list game-name game-save-path game-save-glob) interface)
+         ((:accessors (game-name text-input-pane-text)) game-name)
+         ((:accessors (game-save-path text-input-pane-text)) game-save-path)
+         ((:accessors (game-save-glob text-input-pane-text)) game-save-glob)
+         (selected-id (choice-selection game-list))
+         (selected-game-name (get-collection-item game-list selected-id))
+         ((:accessors (game-list collection-items)) game-list))
+    (remhash selected-game-name games)
+    (setf (gethash game-name games) `(:save-path ,game-save-path
+                                      :save-glob ,game-save-glob)
+          game-list games
+          game-name ""
+          game-save-path ""
+          game-save-glob "")))
+
+(defun remove-game (data interface)
+  (declare (ignore data))
+  (bind (((:slots games game-list game-name game-save-path game-save-glob) interface)
+         ((:accessors (game-name text-input-pane-text)) game-name)
+         ((:accessors (game-save-path text-input-pane-text)) game-save-path)
+         ((:accessors (game-save-glob text-input-pane-text)) game-save-glob)
+         (selected-id (choice-selection game-list))
+         (selected-game-name (get-collection-item game-list selected-id))
+         ((:accessors (game-list collection-items)) game-list))
+    (remhash selected-game-name games)
+    (setf game-list games
+          game-name ""
+          game-save-path ""
+          game-save-glob "")))
 
 (defun start ()
   (display (make-instance 'window)))
