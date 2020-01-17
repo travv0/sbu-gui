319d9fd885b9e836d59e66021595760a2d2d0eb0
Travis <tssund93@gmail.com>
2020-01-17 12:26:57 -0600

    print duration and finish time after backup

--- src/sbu.lisp
+++ src/sbu.lisp
@@ -33,18 +33,43 @@
        hash-table-alist
        (mapcar 'backup-game)))
 
+(defparameter *day-names*
+  '("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"))
+
+(defparameter *month-names*
+  '("Jan" "Feb" "Mar" "Apr" "May"
+    "Jun" "Jul" "Aug" "Sep" "Oct"
+    "Nov" "Dec"))
+
 (tu:desfun backup-game ((game-name . (&key save-path save-glob)))
-  (cl-fad:walk-directory save-path
-                         (curry 'backup-file game-name save-path)
-                         :directories :depth-first
-                         :test (lambda (file)
-                                 (and (not (cl-fad:directory-pathname-p file))
-                                      (pathname-match-p (cl-fad:pathname-as-file file)
-                                                        (path:catfile
-                                                         (cl-fad:pathname-as-directory save-path)
-                                                         (if (string= (or save-glob "") "")
-                                                             "**/*"
-                                                             save-glob)))))))
+  (let ((start-time (get-internal-real-time)))
+    (cl-fad:walk-directory save-path
+                           (curry 'backup-file game-name save-path)
+                           :directories :depth-first
+                           :test (lambda (file)
+                                   (and (not (cl-fad:directory-pathname-p file))
+                                        (pathname-match-p (cl-fad:pathname-as-file file)
+                                                          (path:catfile
+                                                           (cl-fad:pathname-as-directory save-path)
+                                                           (if (string= (or save-glob "") "")
+                                                               "**/*"
+                                                               save-glob))))))
+    (bind ((end-time (get-internal-real-time))
+           ((:values second minute hour date month year day-of-week _ tz)
+            (get-decoded-time)))
+      (format t "~%Finished backing up ~a in ~fs ~
+on ~a, ~a ~d ~d at ~2,'0d:~2,'0d:~2,'0d (GMT~@d)~%~%"
+              game-name
+              (/ (- end-time start-time)
+                 internal-time-units-per-second)
+              (nth day-of-week *day-names*)
+              (nth month *month-names*)
+              date
+              year
+              hour
+              minute
+              second
+              (- tz)))))
 
 (defun backup-file (game-name save-path from)
   (bind ((save-path (cl-fad:pathname-as-directory save-path))
@@ -59,7 +84,7 @@
       (ensure-directories-exist (path:dirname full-to))
       (cl-fad:copy-file from to :overwrite t)
       (cl-fad:copy-file to full-to)
-      (format t "~a ==>~%~4t~a~%" from to))))
+      (format t "~%~a ==>~%~4t~a" from to))))
 
 (defun save-game (games game-name game-save-path game-save-glob &optional old-game-name)
   (remhash old-game-name games)
